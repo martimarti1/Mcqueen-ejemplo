47c3f765c1f7465a4d3033895b21b74349530e31
martimarti1 <martinavillonedayenoff388@gmail.com>
2024-12-11 16:21:49 -0300

    Add files via upload

---
+++ prueba - intento/cars/javascript.js
@@ -0,0 +1,6 @@
+var nombreUsuario = document.getElementById("input-nombre").value;
+var fotoDePerfil = document.getElementById("input-imagen-perfil").value;
+var emailUsuario = document.getElementById("input-email-usuario").value;
+function cargarPerfil(){
+    
+}
